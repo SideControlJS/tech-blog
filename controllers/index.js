99a0a81000458bc75cd6b0c1cffc9f313d5facb3
Jesse <jesse.lare@gmail.com>
2023-12-03 00:17:58 -0500

    updating index.js/packag.json

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,12 @@
 // Path: controllers/index.js
 const router = require("express").Router();
-const apiRoutes = require("./controllers/api");
-const dashboardRoutes = require("./dashboardRoutes");
+const apiRoutes = require("./api");
+const homeRoutes = require("./homeRoutes");
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
-router.use("/dashboard", dashboardRoutes);
+
 
 module.exports = router;
+
 
